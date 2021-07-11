4e0057498163494e8c342e6f73c70cdb0b058bfb
Corey Gumbs <cgumbswebdev@gmail.com>
2021-07-10 22:05:25 -0400

    [Update] Added test for className in Navigation Component

--- src/components/Navigation/__test__/navigation.test.js
+++ src/components/Navigation/__test__/navigation.test.js
@@ -1,12 +1,17 @@
 //Test of Navigation Component
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {getByTestId, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Navigation from '../Navigation';
 
 //test that Navigation component renders
-test('Navigation renders with correct components/text', () => {
+test('Navigation renders with correct class', () => {
     const component = render(<Navigation />);
+    const navElement = component.getByTestId('navigation');
+
+    //expects className = navigation to be true.
+    expect(navElement.className).toBe('navigation');    
+    
     screen.debug();
 })
