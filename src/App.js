8e9fa299307788c73ea2e6fcdbcd39b33d9aa992
Corey Gumbs <cgumbswebdev@gmail.com>
2021-07-19 20:02:15 -0400

    [Add] Added NoMatch/404 component, routing tests all pass.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Navigation from './components/Navigation/Navigation';
+import NoMatch from './components/NoMatch/NoMatch';
 
 function App() {
   return (
     <div className='App'>
-      <Navigation/>
-      You are on main page.
+      <Navigation/> 
       <Switch>
         <Route exact path='/'>
+        </Route>
+        <Route exact>
+          <NoMatch/>
         </Route>
       </Switch>
     </div>
