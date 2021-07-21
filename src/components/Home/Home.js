766d25e4aa349e25ffe3484903ed1f89286f7780
Corey Gumbs <cgumbswebdev@gmail.com>
2021-07-21 15:23:46 -0400

    [Add] Created home component and __test__ dir

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home =  () => {
     return(
-        <div className='home-page'>
+        <div className='home-page' data-testid='home-page'>
             <h2>You are on main page.</h2>
         </div>
     )
