0beccdc44bc9849a385272eeefc621248e52b1a6
Corey Gumbs <cgumbswebdev@gmail.com>
2021-07-20 20:43:20 -0400

    [Fixed] Fixed some test errors.

--- src/components/Navigation/NavMenuItem/__test__/navmenuitem.test.js
+++ src/components/Navigation/NavMenuItem/__test__/navmenuitem.test.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import {cleanup, render, screen} from '@testing-library/react';
+import {cleanup, render, screen, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
+import App from '../../../../App';
 import NavMenuItem from '../NavMenuItem';
+
+import data from '../../NavMenu/data/data.json'
 
 afterEach(cleanup);
 
 describe('NavMenuItem Renders', () => {
    
     test('NavMenuItem renders with props', () => {
+        const myData = data;
         const history = createMemoryHistory();
         render(
             <Router history={ history }>
-                <NavMenuItem itemName={'test'} navRoute={'/test'}/>
+                <NavMenuItem itemName={myData[0].name} navRoute={myData[0].route}/>
             </Router>
             );
         
@@ -22,10 +27,10 @@ describe('NavMenuItem Renders', () => {
         expect(screen.getByTestId('navmenu-item')).toBeInTheDocument();
 
         //Expects a Link to render with props
-        expect(screen.getByRole('link', {name: 'test'})).toHaveTextContent('test');
+        expect(screen.getByRole('link', {name: myData[0].name})).toHaveTextContent(myData[0].name);
         
         //Expects a Link to render route props
-        expect(screen.getByRole('link')).toHaveAttribute('href', '/test');
+        expect(screen.getByRole('link')).toHaveAttribute('href', myData[0].route);
 
         //screen.debug();
     });
